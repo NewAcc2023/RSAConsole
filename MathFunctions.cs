ad8e1ebfc60a3a022ca7cb79933e4959c303b8a0
Alex <newacc4work@outlook.com>
2023-09-27 18:11:31 +0300

    Add project files.

---
+++ MathFunctions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSAConsole
+{
+	internal class MathFunctions
+	{
+
+		public static long FastExponentiation(long number, long power, long n )
+		{
+			long result = 1;
+			for (long i = 0; i < power; i++)
+			{
+				result = result * number % n;
+			}
+			return result;
+		}
+
+		public static int GCD(int a, int b)
+		{
+			if (b == 0)
+				return a;
+			return GCD(b, a % b);
+		}
+
+		public static int EulersTotientFunction(float p1, float p2)
+		{
+			return (int)((p1 - 1) * (p2 - 1));
+		}
+
+		public static List<int> PrimesUpToN(int n)
+		{
+			List<bool> a = Enumerable.Repeat(true, n + 1).ToList();
+			for (float i = 2; i <= Math.Sqrt(n); i++)
+			{
+				if (a[(int)i] == true)
+				{
+					int j = (int)i * (int)i;
+					while (j <= n)
+					{
+						a[j] = false;
+						j = j + (int)i;
+					}
+				}
+			}
+
+			List<int> primes = new List<int>();
+			for (int i = 2; i <= n; i++)
+			{
+				if (a[i] == true)
+				{
+					primes.Add(i);
+				}
+			}
+			return primes;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSAConsole
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int size = 1000;
+			List<int> primes = MathFunctions.PrimesUpToN(size);
+			Random random = new Random();
+			int p1 = primes[random.Next(primes.Count)];
+			int p2 = primes[random.Next(primes.Count)];
+
+			while (p1 == p2)
+				p2 = primes[random.Next(primes.Count)];
+
+			int N = p1 * p2;
+			int FN = MathFunctions.EulersTotientFunction(p1, p2);
+
+			int E = 2;
+			while (MathFunctions.GCD(E, FN) != 1)
+				E++;
+
+			int D = 1;
+			while (D * E % FN != 1)
+				D++;
+
+			string text = "hello abc 12";
+
+			UnicodeEncoding byteConverter = new UnicodeEncoding();
+			Console.WriteLine($"p1 = {p1},p2 = {p2}");
+			Console.WriteLine($"PLAIN TEXT = {text}");
+
+			long[] C = new long[text.Length];
+            for (int i = 0; i<C.Length; i++)
+			{
+				C[i] = MathFunctions.FastExponentiation(Convert.ToInt32(text[i]), E, N);
+			}
+
+			StringBuilder sb = new StringBuilder();
+			int counter = 0;
+			Console.WriteLine($"ENCRYPTED CODES");
+			foreach (var item in C)
+            {
+				counter++;
+				sb.Append(Convert.ToChar((byte)item));
+				
+				if(counter != 8)
+				{
+					Console.Write(item.ToString() + "\t");
+				}
+				else
+				{
+					Console.Write(item.ToString() + "\n");
+					counter = 0;
+				}
+			}
+			Console.WriteLine($"\nCRYPTED = {sb}");
+
+			long[] P = new long[text.Length];
+			for (int i = 0; i < P.Length; i++)
+			{
+				P[i] = MathFunctions.FastExponentiation(Convert.ToInt32(C[i]), D, N);
+			}
+
+			StringBuilder sb2 = new StringBuilder();
+			counter = 0;
+			Console.WriteLine($"DECRYPTED CODES");
+			foreach (var item in P)
+			{
+				counter++;
+				sb2.Append(Convert.ToChar((byte)item));
+				if (counter != 8)
+				{
+					Console.Write(item.ToString() + "\t");
+				}
+				else
+				{
+					Console.Write(item.ToString() + "\n");
+					counter = 0;
+				}
+			}
+			Console.WriteLine($"\nDECRYPTED = {sb2}");
+
+		}
+	}
+}
